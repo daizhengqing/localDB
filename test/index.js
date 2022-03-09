fd612bb0d983e27ae5d88b955799b89fbbafbd59
daizhengqing <daizhengqing@laihua.com>
2022-03-09 11:10:11 +0800

    feat: update

--- test/index.js
+++ test/index.js
@@ -1,9 +1,14 @@
 import DB from '../lib/index.js'
 
-const db = new DB({
+const db = await new DB({
   collection: [
     { name: 'test' }
   ]
-})
+}).init()
+
+db.test.insert({ a:233 })
+db.test.insert({ a:666 })
+db.test.insert({ a:7777 })
+db.test.removeAll({ a:233 })
 
 console.log(db)
